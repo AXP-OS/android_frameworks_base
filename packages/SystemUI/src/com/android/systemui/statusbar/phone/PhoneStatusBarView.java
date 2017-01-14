5f39053372f77a8bfd597ae4f68e71049beffc5f
Park Ju Hyung <qkrwngud825@gmail.com>
2017-01-15 03:33:04 +0900

    SystemUI: add burnIn protection
    Devices with AMOLED display suffer from
    status-bar's notification items and nagivation bar's software keys
    causing permanent burn-ins when used long-term.
    
    Moving all items in the area
    both horizontally and vertically workarounds this problem.
    
    jrizzoli: integrate with runtime cmsdk preference
    
    Linux4: Removed toggle from settings - the burnIn protection is always
    enabled if the corresponding AOSP overlay is set to true
    Updated for T
    
    Change-Id: I35b04d1edff86a556adb3ad349569e5d82653f16
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Thecrazyskull <anaskarbila@gmail.com>
    Signed-off-by: Joey Rizzoli <joey@lineageos.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -19,6 +19,7 @@ package com.android.systemui.statusbar.phone;
 
 import android.annotation.Nullable;
 import android.content.Context;
+import android.content.res.Resources;
 import android.content.res.Configuration;
 import android.graphics.Rect;
 import android.inputmethodservice.InputMethodService;
@@ -63,6 +64,13 @@ public class PhoneStatusBarView extends FrameLayout implements Callbacks {
     private static final String TAG = "PhoneStatusBarView";
     private final CommandQueue mCommandQueue;
     private final StatusBarContentInsetsProvider mContentInsetsProvider;
+
+    private int mBasePaddingBottom;
+    private int mBasePaddingLeft;
+    private int mBasePaddingRight;
+    private int mBasePaddingTop;
+
+    private ViewGroup mStatusBarContents;
 
     private DarkReceiver mBattery;
     private ClockController mClockController;
@@ -137,6 +145,18 @@ public class PhoneStatusBarView extends FrameLayout implements Callbacks {
         mTouchEventHandler = handler;
     }
 
+    public void shiftStatusBarItems(int horizontalShift, int verticalShift) {
+        if (mStatusBarContents == null) {
+            return;
+        }
+
+        mStatusBarContents.setPaddingRelative(mBasePaddingLeft + horizontalShift,
+                mBasePaddingTop + verticalShift,
+                mBasePaddingRight + horizontalShift,
+                mBasePaddingBottom - verticalShift);
+        invalidate();
+    }
+
     void init(StatusBarUserChipViewModel viewModel) {
         StatusBarUserSwitcherContainer container = findViewById(R.id.user_switcher_container);
         StatusBarUserChipViewBinder.bind(container, viewModel);
@@ -148,6 +168,13 @@ public class PhoneStatusBarView extends FrameLayout implements Callbacks {
         mBattery = findViewById(R.id.battery);
         mClockController = new ClockController(getContext(), this);
         mCutoutSpace = findViewById(R.id.cutout_space_view);
+
+        mStatusBarContents = (ViewGroup) findViewById(R.id.status_bar_contents);
+
+        mBasePaddingLeft = mStatusBarContents.getPaddingStart();
+        mBasePaddingTop = mStatusBarContents.getPaddingTop();
+        mBasePaddingRight = mStatusBarContents.getPaddingEnd();
+        mBasePaddingBottom = mStatusBarContents.getPaddingBottom();
 
         updateResources();
     }
