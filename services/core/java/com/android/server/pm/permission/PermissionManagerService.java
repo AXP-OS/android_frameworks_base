ad41b5ccffba4d4c90a53e54a21924bed8f78675
Daniel Micay <danielmicay@gmail.com>
2019-03-17 11:59:15 -0400

    make INTERNET into a special runtime permission

--- services/core/java/com/android/server/pm/permission/PermissionManagerService.java
+++ services/core/java/com/android/server/pm/permission/PermissionManagerService.java
@@ -771,7 +771,7 @@ public class PermissionManagerService {
     }
 
     public static boolean isSpecialRuntimePermission(final String permission) {
-        return false;
+        return Manifest.permission.INTERNET.equals(permission);
     }
 
     private void grantPermissions(PackageParser.Package pkg, boolean replace,
