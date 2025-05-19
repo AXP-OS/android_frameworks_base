8c0f292262d32ed948cfef86b0625e7498ce2243
DivestOS <tavi@divested.dev>
2025-05-19 22:47:53 +0200

    DivestOS script adjustments
    Change-Id: I492939b4d17b226c8d87cf941a4ee3cf2e382386

--- services/core/java/com/android/server/wm/ActivityTaskManagerService.java
+++ services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -6553,7 +6553,7 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
                 } catch (RemoteException e) {
                 }
 
-                if (!Build.isBuildConsistent()) {
+                if (false) {
                     Slog.e(TAG, "Build fingerprint is not consistent, warning user");
                     mUiHandler.post(() -> {
                         if (mShowDialogs) {
